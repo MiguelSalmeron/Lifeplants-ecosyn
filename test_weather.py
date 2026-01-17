bcde986b5f7629ca15263167e058df6fd45ff17b
MiguelSalmeron <Miguel.Salmeron.dev@gmail.com>
2026-01-17 09:26:39 -0600

    changing the language

--- test_weather.py
+++ test_weather.py
@@ -1,9 +1,10 @@
 import os
 import requests
-from dotenvimpot load_dotenv
+from dotenv import load_dotenv
 
 load_dotenv()
 
+# Securely get the API Key
 API_KEY = os.getenv("OPENWEATHER_API_KEY")
 CITY = "Managua"
 
@@ -12,23 +13,22 @@ def get_weather():
         print(" Error: No API Key found. Please set OPENWEATHER_API_KEY in your .env file.")
         return
 
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric&lang=es"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric&lang=en"
     response = requests.get(url)
 
-    
     if response.status_code == 200:
         data = response.json()
         temp = data['main']['temp']
         desc = data['weather'][0]['description']
-        print(f" ¡ÉXITO! En {CITY} hace {temp}°C con {desc}.")
+        print(f"✅ SUCCESS! In {CITY} it is {temp}°C with {desc}.")
         
-        # Lógica simple para Lifeplants
+        # logic for Lifeplantsd
         if temp > 30:
-            print(" ¡ALERTA! Hace mucho calor. La planta necesitará agua pronto.")
+            print(" HEAT ALERT! It's very hot. The plant will need water soon.")
         else:
-            print(" El clima es agradable. La planta está tranquila.")
+            print(" The weather is nice. The plant is chilling.")
     else:
-        print(" Error: Revisa tu API Key o el nombre de la ciudad.")
+        print(" Error: Check your API Key or the city name.")
 
 if __name__ == "__main__":
     get_weather()
